396163b8d28f2451d47de230c696eac9e7caf2c9
waydechou <z.linwei@yahoo.com>
2016-04-30 17:59:20 +0800

    For test
    Git tutorial for test

---
+++ 05_AFUpload/03_AFUpload/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  03_AFUpload
+//
+//  Created by JayWon on 15/10/6.
+//  Copyright (c) 2015年 JayWon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 05_AFUpload/03_AFUpload/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  03_AFUpload
+//
+//  Created by JayWon on 15/10/6.
+//  Copyright (c) 2015年 JayWon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
